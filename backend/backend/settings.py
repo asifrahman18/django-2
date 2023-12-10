a2ba3dc4f12eaa42fde628551105cd0f37309e9b
Asif Rahman <57532856+asifrahman18@users.noreply.github.com>
2023-12-10 13:27:45 +0600

    runserver

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'django.contrib.gis'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'django-test',
+        'USER': 'postgres',
+        'PASSWORD': '18469',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
@@ -121,3 +128,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+VIRTUAL_ENV_BASE =  os.environ.get('VIRTUAL_ENV')
+
+print(VIRTUAL_ENV_BASE)
+
+GDAL_LIBRARY_PATH =  VIRTUAL_ENV_BASE + '/Lib/site-packages/osgeo/gdal304.dll'
+GEOS_LIBRARY_PATH =  VIRTUAL_ENV_BASE + '/Lib/site-packages/osgeo/geos_c.dll'
