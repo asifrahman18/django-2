0d3e81fc26fb044564e0883ff07aca9d1f40424b
Asif Rahman <57532856+asifrahman18@users.noreply.github.com>
2024-01-14 19:13:06 +0600

    user register, user login

--- backend/job/filters.py
+++ backend/job/filters.py
@@ -4,9 +4,11 @@ from .models import Job
 
 class JobsFilter(filters.FilterSet):
     
+    keyword = filters.CharFilter(field_name='title', lookup_expr='icontains')
+    location = filters.CharFilter(field_name='location', lookup_expr='icontains')
     min_salary = filters.NumberFilter(field_name='salary' or 0, lookup_expr='gte')
     max_salary = filters.NumberFilter(field_name='salary' or 100000, lookup_expr='lte')
     
     class Meta:
         model = Job
-        fields = ('title','location', 'qualification', 'jobType', 'min_salary','max_salary')
+        fields = ('keyword','location', 'qualification', 'jobType', 'min_salary','max_salary')
